d3bd237ac1f9393666a33717c0ddc511285e9d98
tja414312570 <tja414312570@gmail.com>
2024-11-15 15:32:34 +0800

    完善readme

--- libs/virtual-window/src/virtual-window-group.ts
+++ libs/virtual-window/src/virtual-window-group.ts
@@ -1,3 +1,4 @@
+import { v4 } from "uuid";
 import VirtualWindow from "./virtual-window";
 type ChildVirtualWindow = InstanceType<typeof VirtualWindow> & {
     _line: boolean | undefined;
@@ -5,7 +6,7 @@ type ChildVirtualWindow = InstanceType<typeof VirtualWindow> & {
 }
 
 export class WindowGroup {
-    private virtualWindowArray: Array<ChildVirtualWindow | string> = [];
+    private virtualWindowArray: Array<ChildVirtualWindow> = [];
     private parentWindow: VirtualWindow;
     private _write: (content: string) => void;
     isClose: boolean;
@@ -38,12 +39,11 @@ export class WindowGroup {
         let buffer = ''
         let isFirst = true;
         for (const virtualWindow of this.virtualWindowArray) {
-            const render = typeof virtualWindow === 'string' ? virtualWindow :
-                virtualWindow.render();
+            const render = virtualWindow.render();
             if (render.length === 0) {
                 continue;
             }
-            if (typeof virtualWindow === 'object' && virtualWindow['_line'] && !isFirst && buffer) {
+            if (virtualWindow['_line'] && !isFirst && buffer) {
                 buffer += '\n';
             }
             isFirst = false;
@@ -52,39 +52,57 @@ export class WindowGroup {
         this._buffer = buffer;
         this._write("\x1b[s\x1b[0J" + buffer + "\x1b[u")
     }
+
+    insertWindow(index: number, virtualWindow?: VirtualWindow) {
+        if (index >= 0 && index <= this.virtualWindowArray.length) {
+            const insertWindow = this._createOrWrapperWindow(virtualWindow)
+            insertWindow['_id'] = insertWindow['_id'] || v4()
+            this.virtualWindowArray.splice(index, 0, insertWindow);
+            this.refresh();
+        } else {
+            throw new Error('插入位置超出窗口组范围');
+        }
+    }
+    getOrder() {
+        return this.virtualWindowArray.map(item => item['_id']);
+    }
     createChildWindow(id?: string) {
         const virtualWindow = this.createChildRowWindow(id);
         virtualWindow['_line'] = true;
         return virtualWindow;
     }
-    createChildRowWindow(id?: string) {
-        this._checkStatus();
-        const i = this.virtualWindowArray.length;
-        const virtualWindow = new VirtualWindow() as unknown as ChildVirtualWindow;
+    private _createOrWrapperWindow(existsWindow?: VirtualWindow) {
+        const virtualWindow = existsWindow || new VirtualWindow();
         virtualWindow.onRender((content) => {
             this.refresh();
         });
+        return virtualWindow as unknown as ChildVirtualWindow;
+    }
+    createChildRowWindow(id?: string) {
+        this._checkStatus();
+        const i = this.virtualWindowArray.length;
+        const virtualWindow = this._createOrWrapperWindow();
         this.virtualWindowArray[i] = virtualWindow;
-        const destory = virtualWindow.destory.bind(virtualWindow);
-        virtualWindow.destory = () => {
-            let render = virtualWindow.render();
-            if (render.length > 0) {
-                if (virtualWindow['_line'] && i > 0) {
-                    render = '\n' + render;
-                }
-            }
-            this.virtualWindowArray[i] = render;
-            destory();
-        }
-        virtualWindow['_id'] = id || i;
+        // const destory = virtualWindow.destory.bind(virtualWindow);
+        // virtualWindow.destory = () => {
+        //     let render = virtualWindow.render();
+        //     if (render.length > 0) {
+        //         if (virtualWindow['_line'] && i > 0) {
+        //             render = '\n' + render;
+        //         }
+        //     }
+        //     this.virtualWindowArray[i] = render;
+        //     destory();
+        // }
+        virtualWindow['_id'] = id || v4();
         return virtualWindow;
     }
     close(force = false) {
         this.isClose = true;
         for (const virtualWindow of this.virtualWindowArray) {
-            if (typeof virtualWindow === 'object') {
+            if (!virtualWindow.isClose()) {
                 if (force) {
-                    virtualWindow.destory();
+                    virtualWindow.close();
                 } else {
                     throw new Error(`子窗口${virtualWindow['_id']}未关闭，不允许关闭组,(传入force强制关闭窗口)`)
                 }
@@ -100,6 +118,15 @@ export class WindowGroup {
         if (!this.isDestory) {
             this.virtualWindowArray.length = 0;
             this.isDestory = true;
+            for (const virtualWindow of this.virtualWindowArray) {
+                if (!virtualWindow.isDestory()) {
+                    if (force) {
+                        virtualWindow.destory();
+                    } else {
+                        throw new Error(`子窗口${virtualWindow['_id']}未销毁，不允许销毁组,(传入force强制销毁窗口)`)
+                    }
+                }
+            }
         }
     }
 }
